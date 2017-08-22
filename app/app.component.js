1ff13614eb9267a3ef4a5e1f2946bf7ea9c38b48
Lucas Stoque <stoque@live.com>
2017-08-21 21:28:56 -0300

    Add root component

---
+++ app/app.component.js
@@ -0,0 +1,13 @@
+export const AppComponent = {
+  template: `
+    <header>
+        My App
+    </header>
+    <div>
+        <div ui-view></div>
+    </div>
+    <footer>
+        Copyright MyApp 2017.
+    </footer>
+  `
+};
--- app/app.module.js
+++ app/app.module.js
@@ -1,11 +1,13 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 
+import { AppComponent } from './app.component';
 import { HelloModule } from './components/hello/hello.module';
 
 angular
   .module('app', [
     uiRouter,
-    HelloModule,
+    HelloModule
   ])
+  .component('app', AppComponent)
   .name
